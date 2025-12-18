357e4ed6017b61b8b624387276f4f171ba653583
renanlimaa <renanliimaa2005@hotmail.com>
2025-12-17 22:28:23 -0300

    feat: add database configuration with environment variable

--- src/com/crm/jdbc/db/DBConfig.java
+++ src/com/crm/jdbc/db/DBConfig.java
@@ -1,4 +1,13 @@
-package jdbc.application.db;
+package com.crm.jdbc.db;
 
 public class DBConfig {
+
+    public static final String URL =
+            "jdbc:mysql://localhost:3306/crm_db";
+
+    public static final String USER = "root";
+
+    public static final String PASSWORD =
+            System.getenv("DB_PASSWORD");
+
 }
