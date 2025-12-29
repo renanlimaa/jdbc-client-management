8bcdb072ad965af9c482ba9754ac136dec0bb95b
renanlimaa <renanliimaa2005@hotmail.com>
2025-12-28 23:56:50 -0300

    Add custom DatabaseException class

--- src/com/crm/jdbc/exception/DatabaseException.java
+++ src/com/crm/jdbc/exception/DatabaseException.java
@@ -1,4 +1,12 @@
-package jdbc.application;
+package com.crm.jdbc.exception;
 
-public class DatabaseException {
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
