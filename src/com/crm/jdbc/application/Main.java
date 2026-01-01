570e068370f649636c2e163bcbe0edc422c4cd33
renanlimaa <renanliimaa2005@hotmail.com>
2025-12-31 21:04:42 -0300

    feat: add full CRUD demonstration to Main class

--- src/com/crm/jdbc/application/Main.java
+++ src/com/crm/jdbc/application/Main.java
@@ -1,15 +1,38 @@
+package com.crm.jdbc.application;
+
 import com.crm.jdbc.dao.ClientDAO;
 import com.crm.jdbc.model.Client;
 
-import java.sql.SQLException;
+public class Main {
+    public static void main(String[] args) {
 
-public static void main(String[] args) throws SQLException {
+        ClientDAO dao = new ClientDAO();
 
-    Client client = new Client("Johnny Test", "johnnytest@gmail.com");
+        System.out.println("\n--- INSERT ---");
+        Client client = new Client("Johnny Test", "johnnytest@gmail.com");
+        dao.insert(client);
+        System.out.println("Client inserted!");
 
-    ClientDAO dao = new ClientDAO();
-    dao.insert(client);
+        System.out.println("\n--- FIND ALL ---");
+        dao.findAll().forEach(c ->
+                System.out.println(c.getId() + " | " + c.getName() + " | " + c.getEmail())
+        );
 
-    System.out.println("Client inserted sucessfully!");
+        System.out.println("\n--- FIND BY ID (1) ---");
+        Client found = dao.findById(1);
+        System.out.println(found != null ?
+                found.getName() + " found!" :
+                "No client found with this ID."
+        );
 
+        System.out.println("\n--- UPDATE EMAIL ---");
+        dao.updateEmail(1, "johnny.updated@gmail.com");
+        System.out.println("Email updated!");
+
+        System.out.println("\n--- DELETE BY ID ---");
+        dao.deleteById(1);
+        System.out.println("Client deleted!");
+
+        System.out.println("\nCRUD executed successfully!");
+    }
 }
